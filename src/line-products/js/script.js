79b67b6b6f61f05674c45cd35539766a76fc48a5
caique.moraes <caique.moraes@compasso.com.br>
2022-02-18 16:16:04 -0300

    line banners

--- src/line-products/js/script.js
+++ src/line-products/js/script.js
@@ -1,32 +1,33 @@
-$('.responsive').slick({
-  dots: true,
+$('[data-carousel="line-banner"]').slick({
   infinite: false,
   speed: 300,
-  slidesToShow: 4,
-  slidesToScroll: 4,
+  slidesToShow: 6,
+  slidesToScroll: 3,
+  dots: false,
+  infinite: true,
   responsive: [
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 3,
+        arrows: true,
+        slidesToShow: 4,
         slidesToScroll: 3,
-        infinite: true,
-        dots: true
       }
     },
     {
       breakpoint: 600,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 2
+        slidesToScroll: 2,
+        dots: true
       }
     },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
+    // {
+    //   breakpoint: 480,
+    //   settings: {
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1
+    //   }
+    // }
   ]
 })
