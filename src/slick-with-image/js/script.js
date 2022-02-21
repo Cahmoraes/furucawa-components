0b96ce4c1d5a859b5aec6a3841ab9c4aab53e814
marlon.chiodelli@compasso.com.br <marlon.chiodelli@compasso.com.br>
2022-02-21 17:35:02 -0300

    Vitrine de produtos com imagem

---
+++ src/slick-with-image/js/script.js
@@ -0,0 +1,45 @@
+$('[data-carousel="product-with-image"]').slick({
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  dots: false,
+  infinite: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        arrows: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      }
+    },
+    {
+      breakpoint: 760,
+      settings: {
+        arrows: false,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 491,
+      settings: {
+        arrows: false,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 490,
+      settings: {
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true
+      }
+    }
+  ]
+})
